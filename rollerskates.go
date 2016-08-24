822a84d1fde6221c2a8d92d10d1e168bc9b9eb0e
Craig Bass <craig@madetech.com>
2016-08-24 16:29:04 +0100

    main

--- rollerskates.go
+++ rollerskates.go
@@ -1,1 +1,21 @@
 package rollerskates
+
+import (
+	"os"
+	"github.com/urfave/cli"
+	"fmt"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "rollerskates"
+	app.Usage = "rollerskates load-balancer-name"
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Bear with me whilst I put my skates on...")
+		//RestartLoadBalancerInstances(c.Args().Get(0))
+		return nil
+	}
+
+	app.Run(os.Args)
+}
